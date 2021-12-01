932865e42d3a506c6ac02dcbbd0eaa43419abaf3
Albert <a19albchavas@inspedralbes.cat>
2021-12-01 10:45:05 +0100

    Base de datos principal

---
+++ back/sql/tardium.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-12-2021 a las 10:43:55
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tardium`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id_user` int(9) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `accounts`
+--
+
+INSERT INTO `accounts` (`id_user`, `username`, `email`, `password`) VALUES
+(1, 'user1', 'user1@gmail.com', 'user1'),
+(2, 'user2', 'user2@gmail.com', 'user2'),
+(3, 'user3', 'user3@gmail.com', 'user3'),
+(4, 'user4', 'user4@gmail.com', 'user4');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `feedbacks`
+--
+
+CREATE TABLE `feedbacks` (
+  `id_film` int(9) NOT NULL,
+  `id_user` int(9) NOT NULL,
+  `rating` float NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `films`
+--
+
+CREATE TABLE `films` (
+  `id_film` int(9) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `year` int(4) NOT NULL,
+  `rating` float NOT NULL,
+  `poster` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `films`
+--
+
+INSERT INTO `films` (`id_film`, `title`, `year`, `rating`, `poster`) VALUES
+(15, 'Cars', 2005, 4.5, 'www.images.com/cars');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `accounts`
+--
+ALTER TABLE `accounts`
+  ADD UNIQUE KEY `id` (`id_user`);
+
+--
+-- Indices de la tabla `films`
+--
+ALTER TABLE `films`
+  ADD UNIQUE KEY `id_film` (`id_film`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id_user` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
