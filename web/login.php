61172c9765344245e9a30e346b1d5be036ce99a9
Albert <a19albchavas@inspedralbes.cat>
2021-12-02 17:33:47 +0100

    Revert "Database Connection"
    This reverts commit de46c168983ae62dec1ec26971ad96d1c67c09a6.

--- web/login.php
+++ web/login.php
@@ -1,15 +1,9 @@
+
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Content-type: application/json');
-    require_once(__DIR__."/../server/accountSystem/AccountManager.php");
-    /*========Save the info we got from POST==========*/
     $email = $_POST["email"];
     $password = $_POST["password"];
-    /*========We create the AccountManager Object==========*/
-    $AccountManager = new AccountManager();
-    /*========Set the email to AccountManager==========*/
-    $AccountManager->setEmail($email);
-    /*========We call the database to recive the account==========*/
-    $data = $AccountManager->LogIn();
-    echo json_encode($data);
+    $array = array("email"=>$email, "password"=>$password);
+    echo json_encode($array);
 ?>
