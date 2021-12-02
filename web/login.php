de46c168983ae62dec1ec26971ad96d1c67c09a6
Eric <60893006+a19ericlecas@users.noreply.github.com>
2021-12-02 16:25:40 +0100

    Database Connection

--- web/login.php
+++ web/login.php
@@ -1,9 +1,15 @@
-
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Content-type: application/json');
+    require_once(__DIR__."/../server/accountSystem/AccountManager.php");
+    /*========Save the info we got from POST==========*/
     $email = $_POST["email"];
     $password = $_POST["password"];
-    $array = array("email"=>$email, "password"=>$password);
-    echo json_encode($array);
+    /*========We create the AccountManager Object==========*/
+    $AccountManager = new AccountManager();
+    /*========Set the email to AccountManager==========*/
+    $AccountManager->setEmail($email);
+    /*========We call the database to recive the account==========*/
+    $data = $AccountManager->LogIn();
+    echo json_encode($data);
 ?>
