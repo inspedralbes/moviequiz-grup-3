98cc63aab1bae45148f7b6507fa4e4800a4f5cfe
EricCC <60893006+a19ericlecas@users.noreply.github.com>
2021-12-15 10:08:19 +0100

    UX en registration

--- web/authentication.js
+++ web/authentication.js
@@ -86,13 +86,37 @@ function Login() {
 
 //#region REGISTER FORM
 const registerUsername = document.getElementById("reg-username");
+let registerUsernameHelperText = registerUsername.nextElementSibling;
+
 const registerEmail = document.getElementById("reg-email");
+let registerEmailHelperText = registerEmail.nextElementSibling;
+
 const registerPassword = document.getElementById("reg-password");
+let registerPasswordHelperText = registerPassword.nextElementSibling;
+
+const registerRepeatedPassword = document.getElementById("reg-rpassword");
 let registerButton = document.getElementById("register-button");
 
 /*==========We add the events needed for login==========*/
+registerUsername.addEventListener('blur', () =>{
+    CheckUsername();
+    Validate();
+});
+registerEmail.addEventListener('blur', () =>{
+    CheckEmail();
+    Validate();
+});
+registerPassword.addEventListener('blur', () =>{
+    CheckPasswords();
+    Validate();
+});
+registerRepeatedPassword.addEventListener('blur', () =>{
+    CheckPasswords();
+    Validate();
+});
 registerButton.addEventListener('click', () => {
     Register();
+    Validate();
 });
 
 /*==========function that will be executed after clicking register button==========*/
@@ -112,6 +136,80 @@ function Register() {
             /*==========response we recive from server==========*/
             console.log(data);
     });
+}
+/*========== Errors ==========*/
+function CheckUsername(){
+
+    let usernameRegex = /[ `!@#$%^&*()+\=\[\]{};':"\\|,.<>\/?~]/;
+    console.log(registerUsername.value.length);
+    console.log(registerUsername.getAttribute('data-length'));
+    console.log(registerEmailHelperText);
+    console.log(usernameRegex.test(registerUsername.value));
+
+    if(registerUsername.value.length == 0){
+        isEmpty(registerUsername);
+    }
+    else if (registerUsername.value.length <= 3) 
+    { 
+        isNotValid(registerUsername);
+        registerUsernameHelperText.setAttribute('data-error',`El nom no pot tindre menys de 3 caràcters`);
+    } 
+    else if (registerUsername.value.length > registerUsername.getAttribute('data-length')) 
+    { 
+        isNotValid(registerUsername);
+        registerUsernameHelperText.setAttribute('data-error',`Has superat el maxim de caràcters!`);
+    } 
+    else if(usernameRegex.test(registerUsername.value))
+    {
+        isNotValid(registerUsername);
+        registerUsernameHelperText.setAttribute('data-error',"Caràcters permesos: a-z, A-Z, 0-9, -, _");
+    }
+    else if(registerUsername.value.length > 3 && registerUsername.value.length <= registerUsername.getAttribute('data-length')){ 
+        isValid(registerUsername);
+        registerUsernameHelperText.setAttribute('data-error',"");
+    }
+}
+function CheckEmail(){
+    let emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    if(registerEmail.value.length == 0)
+    {
+        isEmpty(registerEmail)
+    }
+    else if(!emailRegex.test(registerEmail.value))
+    {
+        isNotValid(registerEmail);
+        registerUsernameHelperText.setAttribute('data-error',`El format d'email introduït no és valid`);
+    }
+    else{
+        isValid(registerEmail);
+        registerUsernameHelperText.setAttribute('data-error',"");
+    }
+}
+function CheckPasswords(){
+    if((registerPassword.value.length == 0) && (registerRepeatedPassword.value.length == 0))
+    {
+        isEmpty(registerPassword);
+        isEmpty(registerRepeatedPassword);
+    }
+    else if(registerPassword.value !== registerRepeatedPassword.value){
+        isNotValid(registerPassword);
+        isNotValid(registerRepeatedPassword);
+        registerPasswordHelperText.setAttribute('data-error',"Les contrasenyes no coincideixen");
+    }
+    else if(registerPassword.value === registerRepeatedPassword.value){
+        isValid(registerPassword);
+        isValid(registerRepeatedPassword);
+    }
+}
+function Validate(){
+    if(registerUsername.classList.contains('valid') && registerEmail.classList.contains('valid') && registerPassword.classList.contains('valid') && registerRepeatedPassword.classList.contains('valid'))
+    {
+        registerButton.disabled = false;
+    }
+    else if(!registerButton.disabled)
+    {
+        registerButton.disabled = true;
+    }
 }
 //#endregion
 
@@ -136,6 +234,23 @@ function LogOut(){
 }
 //#endregion
 
+/*========== functions used on input validations ==========*/
+function isValid(input){
+    input.classList.add('valid'); 
+    input.classList.remove('invalid');
+}
+
+function isNotValid(input){
+    input.classList.add('invalid');
+    input.classList.remove('valid'); 
+}
+
+function isEmpty(input){
+    input.classList.remove('invalid');
+    input.classList.remove('valid'); 
+}
+
+
 /*==========function called when you log-in ==========*/
 function isLogged(){
     loginContainer.hidden = true;
@@ -148,5 +263,5 @@ document.addEventListener('DOMContentLoaded', function () {
     M.CharacterCounter.init(textNeedCount);
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
-});
+})
 
